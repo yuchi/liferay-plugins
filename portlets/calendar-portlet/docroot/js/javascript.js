93f3b20e7bf06c14d7a32c935e0c1656a9f67bd9
Pier Paolo Ramon <ramonpierre@gmail.com>
2013-05-24 18:26:04 +0200

    LPS-35264 Implemented the Trash Handler for Calendar Bookings

--- portlets/calendar-portlet/docroot/js/javascript.js
+++ portlets/calendar-portlet/docroot/js/javascript.js
@@ -216,7 +216,7 @@ AUI.add(
 
 				instance.invokeService(
 					{
-						'/calendar-portlet/calendarbooking/delete-calendar-booking': {
+						'/calendar-portlet/calendarbooking/move-calendar-booking-to-trash': {
 							calendarBookingId: schedulerEvent.get('calendarBookingId')
 						}
 					},
@@ -800,7 +800,7 @@ AUI.add(
 		Liferay.CalendarUtil = CalendarUtil;
 
 		var CalendarWorkflow = {
-			STATUS_MAYBE: 8
+			STATUS_MAYBE: 9
 		};
 
 		A.mix(CalendarWorkflow, Workflow);
